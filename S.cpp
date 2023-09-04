294de5b628b9add5ecdb43d4aaae62a2587c347d
indirectDirectEnumeration69 <108570992+indirectDirectEnumeration69@users.noreply.github.com>
2023-09-04 17:05:31 +0100

    Add files via upload

---
+++ S.cpp
@@ -0,0 +1,22 @@
+#include "ServerM.h"
+#pragma comment(lib, "Ws2_32.lib")
+
+int main(){Server();}
+
+/*
+
+list of includes going to be used : 
+
+#include <thread>
+#include <fstream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <fstream>
+#include <windows.h>
+#include <cstdlib>
+#include <ctime>
+#include <sstream>
+#include <vector>
+#include <cstdint>
+#include <bitset>
+*/
