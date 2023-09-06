ad6105c151942afd1b5ae7261471a297d5920a40
indirectDirectEnumeration69 <108570992+indirectDirectEnumeration69@users.noreply.github.com>
2023-09-06 01:16:00 +0100

    Add files via upload

---
+++ Algo.h
@@ -0,0 +1,89 @@
+#pragma once
+
+//start of region
+#ifndef ALGO //if not defined
+#define ALGO 0000000000 //define
+#endif
+// end of region
+
+
+//new region 
+#if ALGO > 0 //if greater than zero redefine
+#undef ALGO //undefine
+#define ALGO 0000000000 //redefine as starting default val
+#endif //end
+constexpr int ALGval = ALGO; //const equal to macro on comp time
+//end of region
+
+
+//new region
+#ifndef ALGOKEY 
+#define ALGOKEY ALGO // ALGOKEY defined same as ALGO val
+#elif ALGOKEY != ALGO && ALGO > 0
+#undef ALGOKEY
+#undef ALGO
+#define ALGO 0000000000
+#define ALGOKEY ALGO
+#endif
+// end of region
+
+/*
+functions as macros before runtime.
+*/
+
+//new region
+#define KEYSECURE false //pre-set to false
+
+#if KEYSECURE //if key is false
+#define CHECK_KEY(KEYSECURE) "key not encrypted" //macro check key holds false prints "".  
+#define KEY_SECURITY_STATE KEYSECURE //define key sec state as being bool for false to be retrieved on runtime.
+#define SECURE_THE_KEY(x) ((x)*(x)) //simple for now
+#undef KEYSECURE 
+#define KEYSECURE true
+#else //else if keysecure is true
+#define KEY_SECURITY_STATE KEYSECURE //define key sec state as being bool for true to be retrieved on runtime.
+#define CHECK_KEY(KEYSECURE) "Key is encrypted"
+#endif
+//end of region 
+
+struct ALGOs {
+
+
+	ALGOs() {
+
+	}
+	~ALGOs() {
+
+	}
+};
+
+
+
+struct Key {
+	struct ID {
+
+		ID() {
+
+		}
+		~ID() {
+
+		}
+	};
+	Key() {
+
+	}
+	~Key() {
+
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
